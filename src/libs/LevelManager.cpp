dc6863b13e9bef0fb526cfb64d0d6a52fa6efd38
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2025-04-09 13:46:28 +0200

    Add simulation example and update level boundary in LevelManager

--- src/libs/LevelManager.cpp
+++ src/libs/LevelManager.cpp
@@ -38,7 +38,7 @@ LevelManager::LevelManager() {
             GravitySource({400, 600}, 20.f, 150.f, 0.f),
             GravitySource({600, 500}, 25.f, 180.f, -45.f)
         },
-        sf::Vector2f(1450, 100)
+        sf::Vector2f(1550, 160)
     );
 }
 
